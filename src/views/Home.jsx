785e9e7f433a4520e6cafae52fd2bdad63c02a66
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-12-06 12:23:49 +0100

    ♻️ Adjust Home mobile padding and title

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Home() {
 	return (
 		<section className="home" aria-labelledby="home__title">
-			<h1 className="home__title h3">Die neuesten Episoden</h1>
+			<h1 className="home__title h3">Neueste Episoden</h1>
 			<CardList limit={9} />
 			<Link to="/archive" className="home__link">
 				Alle Episoden
